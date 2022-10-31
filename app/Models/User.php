f6f1d54376932faa026306ffa622ddbea8f053ce
wrodriguess77 <willrodrigues4@gmail.com>
2022-10-31 15:04:06 -0300

    Fazendo o relacionamento de usuário com carteira

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
 }
