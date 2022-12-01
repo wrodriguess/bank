4edc140dd3546704168a0a762e80e353a5374444
wrodriguess77 <willrodrigues4@gmail.com>
2022-12-01 19:18:27 -0300

    Camada service

---
+++ app/Providers/TransactionServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\TransactionRepository;
+use Illuminate\Support\ServiceProvider;
+
+class TransactionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(TransactionRepositoryInterface::class, TransactionRepository::class);
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
